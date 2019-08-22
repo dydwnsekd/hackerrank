0d9b0fdeaca6e21babcc83735d09a6dc5e4a5e81
dydwnsekd <dydwnsekd123@gmail.com>
2019-08-22 17:00:23 +0900

    hackerrank Mini-Max Sum python으로 해결

---
+++ python/Mini-Max Sum.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    arr.sort()
+    min_sum = sum(arr[:-1])
+    max_sum = sum(arr[1:])
+
+    print("%d %d" % (min_sum, max_sum))
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
