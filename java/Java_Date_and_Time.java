c245890f671a04962eb1d60e4b3d86ffae03fc29
yj.han <dydwnsekd123@gmail.com>
2019-09-01 20:46:54 +0900

    hackerrank Java Data and Time 해결

--- java/Java_Date_and_Time.java
+++ java/Java_Date_and_Time.java
@@ -28,9 +28,11 @@ class Result {
         String weekdays[] = dfs.getWeekdays();
         
         Calendar cal = new GregorianCalendar(Locale.US);
-        cal.set(Integer.parseInt(year), Integer.parseInt(month)-1, Integer.parseInt(day));
+        cal.set(year, month-1, day);
 
         System.out.println(weekdays[cal.get(Calendar.DAY_OF_WEEK)].toUpperCase());
+
+        return weekdays[cal.get(Calendar.DAY_OF_WEEK)].toUpperCase();
     }
 
 }
