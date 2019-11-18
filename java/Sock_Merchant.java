f63a40ef362f0d4db93e49013dd9cf6ca8f9359c
yj.han <dydwnsekd123@gmail.com>
2019-11-18 23:46:54 +0900

    hackerrank Sock_Merchant 아이디어 작성 다시 생각해야 함

---
+++ java/Sock_Merchant.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+
+        int result = 0;
+        ArrayList<Integer> temp_list = new ArrayList<Integer>();
+
+        // java contains를 이용해서 배열안에 값이 들어있는지 확인하려 했으나, 자바 배열은 기본적으로 contains를 지원하지 않음.
+        // 다른 방법 필요
+        for(int i=0; i<n; i++)
+        {
+            if temp_list.contain(ar[i])
+            {
+                
+            }
+        }
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
