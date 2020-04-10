fe8db0a89f64e3391bcd1fdad85595bd6fbddb78
dydwnsekd <dydwnsekd123@gmail.com>
2020-04-10 18:23:33 +0900

    List Replication 해결

--- scala/List_Replication.scala
+++ scala/List_Replication.scala
@@ -1,3 +1,3 @@
  // https://www.hackerrank.com/challenges/fp-list-replication/problem
  
- def f(num:Int,arr:List[Int]):List[Int] = List.fill(num)(arr).flatten
+def f(num:Int,arr:List[Int]):List[Int] = arr.flatten(List.fill(num)(_))
