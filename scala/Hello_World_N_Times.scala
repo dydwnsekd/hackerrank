e112da4bbbb66c0dd3c4004cb109c76b22c78444
dydwnsekd <dydwnsekd123@gmail.com>
2020-04-11 23:53:22 +0900

    best 방식 설명 주석

--- scala/Hello_World_N_Times.scala
+++ scala/Hello_World_N_Times.scala
@@ -9,6 +9,8 @@ object Solution extends App {
 
 /* best
 object Solution extends App {
+    // scala에서는 문자열에 * 기호를 사용할 수 있어 해당 횟수만큼 반복
+    // 익명 함수를 선언하여 input 값에 따라 Hello World를 input값 만큼 반복
     def f(n: Int) = println("Hello World\n" * n)
 }
 */
