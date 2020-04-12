092d119bbf9c3c4540daffc5e15fd4c2966b805b
dydwnsekd <dydwnsekd123@gmail.com>
2020-04-12 23:56:09 +0900

    List Replication best code 추가 다른 점 잘 모르겠음....

--- scala/List_Replication.scala
+++ scala/List_Replication.scala
@@ -1,3 +1,7 @@
  // https://www.hackerrank.com/challenges/fp-list-replication/problem
  
 def f(num:Int,arr:List[Int]):List[Int] = arr.flatten(List.fill(num)(_))
+
+/* best
+def f(num:Int,arr:List[Int]):List[Int] = arr.flatMap(List.fill(num)(_))
+*/
