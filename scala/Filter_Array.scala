4980f3a4efcccd8e56f885d8647733e25a2c248b
dydwnsekd <dydwnsekd123@gmail.com>
2020-04-16 23:33:53 +0900

    Filter Array best code 추가

--- scala/Filter_Array.scala
+++ scala/Filter_Array.scala
@@ -1,3 +1,7 @@
 // https://www.hackerrank.com/challenges/fp-filter-array/problem
 
 def f(delim:Int,arr:List[Int]):List[Int] = arr.filter(_ < delim)
+
+/* best
+ def f(delim:Int,arr:List[Int]):List[Int] = for (i <- arr if i < delim) yield i
+ /*
