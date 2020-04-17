71d7e34502dfe954ca126c23f43811e4e1e646a7
dydwnsekd <dydwnsekd123@gmail.com>
2020-04-17 23:50:27 +0900

    hackerrank filter positions_in_a_list zipwithindex 사용 설명까지

---
+++ scala/Filter_Positions_in_a_List.scala
@@ -0,0 +1,11 @@
+//https://www.hackerrank.com/challenges/fp-filter-positions-in-a-list/problem
+
+
+def f(arr:List[Int]):List[Int] = {
+    arr.zipWithIndex.unzip._2
+
+    // _1 = value만 나옴 _2 = index만 나옴
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
+*/
+
+}
