425c2a1accbbabbed3d30fccb167cbac14f8d103
dydwnsekd <dydwnsekd123@gmail.com>
2020-04-19 21:44:08 +0900

    Filter Positions in a list 해결

--- scala/Filter_Positions_in_a_List.scala
+++ scala/Filter_Positions_in_a_List.scala
@@ -2,7 +2,7 @@
 
 
 def f(arr:List[Int]):List[Int] = {
-    arr.zipWithIndex.unzip._2
+    arr.zipWithIndex.filter(_._2 % 2 == 1).unzip._1
 
     // _1 = value만 나옴 _2 = index만 나옴
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
