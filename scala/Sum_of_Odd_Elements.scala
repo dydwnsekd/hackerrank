9d550f6ab8b07934fa95701e0375eebfe590a9ea
dydwnsekd <dydwnsekd123@gmail.com>
2020-05-19 23:01:02 +0900

    sum of odd elements zipwithindex 제거 테스트 케이스 1,2 여전히 통과 못함

--- scala/Sum_of_Odd_Elements.scala
+++ scala/Sum_of_Odd_Elements.scala
@@ -1,1 +1,4 @@
- def f(arr:List[Int]):Int = arr.zipWithIndex.filter(_._1 % 2 == 1).unzip._1.sum
+ //def f(arr:List[Int]):Int = arr.zipWithIndex.filter(_._1 % 2 == 1).unzip._1.sum
+ 
+ def f(arr:List[Int]):Int = arr.filter(_ % 2 == 1).sum
+ 
