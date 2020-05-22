22d71f53a12435134385bdc2f7893ac03da3e8ab
dydwnsekd <dydwnsekd123@gmail.com>
2020-05-22 17:43:34 +0900

    hackerrank Array Of N Elements 해결

--- scala/Array_Of_N_Elements.scala
+++ scala/Array_Of_N_Elements.scala
@@ -1,10 +1,10 @@
 object Solution extends App {
+    val readInt = scala.io.StdIn.readInt
 
     def f(num:Int) : List[Int] = {
         val list = (0 until num).toList
 
         return list
-    }
+    }Arr
 
     println(f(readInt))
-}
