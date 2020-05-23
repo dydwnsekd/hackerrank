40cb0b1c608ab3081ebdbb400c48e1e6a8c1f382
dydwnsekd <dydwnsekd123@gmail.com>
2020-05-23 23:31:02 +0900

    Sum of Odd Elements != 0을 이용해 해결

--- scala/Sum_of_Odd_Elements.scala
+++ scala/Sum_of_Odd_Elements.scala
@@ -1,4 +1,5 @@
 //def f(arr:List[Int]):Int = arr.zipWithIndex.filter(_._1 % 2 == 1).unzip._1.sum
 //def f(arr:List[Int]):Int = arr.filter(_ % 2 == 1).sum
- 
-def f(arr:List[Int]):Int = arr.filter(_.abs % 2 == 1).sum
+//def f(arr:List[Int]):Int = arr.filter(_.abs % 2 == 1).sum
+
+def f(arr:List[Int]):Int = arr.filter(_ % 2 != 0).sum
