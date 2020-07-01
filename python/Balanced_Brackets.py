18aa6472c60a7b8a6ad9d4f59207d63797742809
dydwnsekd <dydwnsekd123@gmail.com>
2020-07-01 17:15:06 +0900

    Balanced_Brackets 수정 로직 개선

--- python/Balanced_Brackets.py
+++ python/Balanced_Brackets.py
@@ -1,5 +1,4 @@
 #!/bin/python3
-# https://www.hackerrank.com/challenges/balanced-brackets/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
 
 import math
 import os
@@ -10,19 +9,13 @@ import sys
 # Complete the isBalanced function below.
 def isBalanced(s):
     split_s = []
+    match_bracket = ['()', '[]', '{}']
     for i in s:
         if i in ['{', '(', '[']:
             split_s.append(i)
-        else:
-            if len(split_s) == 0:
-                return 'NO'
-            temp_str = split_s.pop()
-            if temp_str == '{' and i == '}': pass
-            elif temp_str == '[' and i == ']': pass
-            elif temp_str == '(' and i == ')': pass
-            else:
-                return 'NO'
-    
+        elif len(split_s) == 0 or split_s.pop() + i not in match_bracket:
+            return 'NO'
+
     if len(split_s) > 0:
         return 'NO'
     return 'YES'
