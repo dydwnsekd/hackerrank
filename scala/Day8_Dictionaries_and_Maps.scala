4cd1f0eb16d4301695451a7ff73b158262623fd3
dydwnsekd <dydwnsekd123@gmail.com>
2020-07-11 21:18:48 +0900

    scala Dictionaries and maps testcase1번 틀림

---
+++ scala/Day8_Dictionaries_and_Maps.scala
@@ -0,0 +1,30 @@
+import scala.collection.mutable
+
+object Solution {
+
+    def main(args: Array[String]) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution
+*/
+        var m = mutable.Map[String,Int]()
+        val n = scala.io.StdIn.readInt()
+        for ( i <- 0 until n )
+        {
+            val temp = scala.io.StdIn.readLine.split(" ")
+            
+            var name = temp(0)
+            var phone_num = temp(1).toInt
+
+            m += (name -> phone_num)
+        }
+        
+        for ( i <- 0 until n )
+        {
+            var findkey = scala.io.StdIn.readLine()
+            var result = m.getOrElse(findkey, "")
+            if (result == "")
+                println("Not found")
+            else
+                println(findkey+"="+result)
+        }
+    }
+}
