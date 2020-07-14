9e5829f866bd71a7505deeae9461b91bd50a4ef6
dydwnsekd <dydwnsekd123@gmail.com>
2020-07-14 23:23:47 +0900

    toBinaryString 형식 비교가 안되는 문제와 코드가 스칼라스럽지 않음 수정 필요

---
+++ scala/Day10_Binary_Numbers.scala
@@ -0,0 +1,43 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+
+object Solution {
+
+
+
+    def main(args: Array[String]) {
+        val stdin = scala.io.StdIn
+
+        val n = stdin.readLine.trim.toInt
+
+        val bin_n = n.toBinaryString
+        var temp = 0
+        var count = 0
+
+        for (i <- bin_n)
+        {   
+            if (i==1)
+            {
+                print (i)
+                temp += 1
+            }
+            else
+            {   
+                if (count < temp)
+                {
+                    count = temp
+                    temp = 0
+                }
+            }
+        }
+
+        print (count)
+    }
+}
