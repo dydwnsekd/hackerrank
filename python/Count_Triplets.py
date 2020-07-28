19a5e1ab848a3ad6516429e38d34379d45544501
dydwnsekd <dydwnsekd123@gmail.com>
2020-07-28 18:13:45 +0900

    참고 자료코드 이용 Count_Triplets python으로 완료

--- python/Count_Triplets.py
+++ python/Count_Triplets.py
@@ -7,16 +7,20 @@ import re
 import sys
 from collections import Counter
 
-# 참고
-# https://www.thepoorcoder.com/hackerrank-count-triplets-solution/
-
 # Complete the countTriplets function below.
 def countTriplets(arr, r):
-    arr_dict = Counter(r)
-
+    arr_counter = Counter(arr)
+    past_counter = Counter()
+    s = 0
     for i in arr:
-        if i % r == 0:
-            pass
+        j = i//r
+        k = i*r
+        arr_counter[i]-=1
+        if past_counter[j] and arr_counter[k] and not i%r:
+            s+=past_counter[j]*arr_counter[k]
+        past_counter[i]+=1
+    return s
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
