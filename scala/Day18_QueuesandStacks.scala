f4e665e1c1ffa0d6a9b638e6564adcb7d70de603
dydwnsekd <dydwnsekd123@gmail.com>
2020-08-14 23:41:35 +0900

    Day18_QueuesandStacks scala queue, stack로 해결

--- scala/Day18_QueuesandStacks.scala
+++ scala/Day18_QueuesandStacks.scala
@@ -63,3 +63,61 @@ object Solution {
         }
     }
 }
+
+------------
+
+import scala.collection.mutable.Stack
+import scala.collection.mutable.Queue
+import scala.util.control._
+
+class Solution {
+  
+  //Write your code here
+  var stack = new Stack[Char]()
+  var queue = new Queue[Char]()
+
+  def pushCharacter(s : Char) = stack.push(s)
+  def popCharacter() = stack.pop()
+
+  def enqueueCharacter(s : Char) = queue.enqueue(s)
+  def dequeueCharacter() = queue.dequeue()
+}
+
+object Solution {
+    def main(args: Array[String]) {
+        // read the string s
+        var s=scala.io.StdIn.readLine();
+        // create the Solution class object p
+        var obj=new Solution();
+        var i=0;
+        var len=s.length();
+        //push/enqueue all the characters of string s to stack
+        for(i<- 0 to len-1){
+            obj.pushCharacter(s.charAt(i));
+            obj.enqueueCharacter(s.charAt(i));
+        }
+      
+        var isPalindrome=true;
+        /*pop the top character from stack
+          dequeue the first character from queue
+          compare both the characters*/
+
+        val loop = new Breaks;
+        loop.breakable {
+            for(i<- 0 to len/2){
+                if(obj.popCharacter()!=obj.dequeueCharacter()){
+                    isPalindrome=false;
+                    loop.break;
+                }
+            }
+        }
+      
+        //finally print whether string s is palindrome or not
+        if(isPalindrome){
+            println("The word, "+s+", is a palindrome." );
+        }
+        else{
+            println("The word, "+s+", is not a palindrome." );
+        }
+    }
+}
