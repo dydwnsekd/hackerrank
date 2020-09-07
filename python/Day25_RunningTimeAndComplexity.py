68aa29e12971bc8af352ddcbee2c63a66d83d317
dydwnsekd <dydwnsekd123@gmail.com>
2020-09-07 21:55:40 +0900

    Running Time And Complexity python으로 완료 시간 초과 문제 해결

--- python/Day25_RunningTimeAndComplexity.py
+++ python/Day25_RunningTimeAndComplexity.py
@@ -1,23 +1,17 @@
 import sys
-
-def get_prime_list(n):
-    sieve = [True] * n
-
-    m = int(n ** 0.5)
-    for i in range(2, m + 1):
-        if sieve[i] == True:
-            for j in range(i+i, n, i):
-                sieve[j] = False
-
-    return [i for i in range(2, n) if sieve[i] == True]
+# 참고 https://myjamong.tistory.com/139
 
 def check_prime(num):
-    prime_list = get_prime_list(num+1)
-
-    if num in prime_list:
+    if num == 1:
+        return False
+    elif num <= 3:
         return True
     else:
-        return False
+        end_num = int(num ** 0.5) + 1
+        for i in range(2, end_num):
+            if num % i == 0:
+                return False
+        return True
 
 n = int(sys.stdin.readline())
 
