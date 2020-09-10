87a6efffb031215555927d0c1eeb3cf6f7f3d44a
dydwnsekd <dydwnsekd123@gmail.com>
2020-09-10 22:30:34 +0900

    NestedLogin python으로 해결 Wrong answer 문제까지 완료

--- python/Day26_NestedLogic.py
+++ python/Day26_NestedLogic.py
@@ -3,15 +3,17 @@ import sys
 d1, m1, y1 = list(map(int, sys.stdin.readline().split(" ")))
 d2, m2, y2 = list(map(int, sys.stdin.readline().split(" ")))
 
-if y1 < y2:
-    print (0)
-elif m1 < m2:
-    print (0)
-elif y1 > y2:
+if y1 > y2:
     print(10000)
-elif m1 > m2:
-    print ((m1-m2) * 500)
-elif d1 > d2:
-    print ((d1-d2) * 15)
+elif y1 == y2:
+    if m1 > m2:
+        print ((m1-m2) * 500)
+    elif m1 == m2:
+        if d1 > d2:
+            print ((d1-d2) * 15)
+        else:
+            print (0)
+    else:
+        print (0)
 else:
     print (0)
