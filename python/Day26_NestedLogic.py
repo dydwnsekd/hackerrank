ab09d0e973aaf27f7c76943525ff7249c054870e
dydwnsekd <dydwnsekd123@gmail.com>
2020-09-11 22:47:25 +0900

    scala NestedLogic 완료

--- python/Day26_NestedLogic.py
+++ python/Day26_NestedLogic.py
@@ -1,19 +1,23 @@
-import sys
+object Solution {
 
-d1, m1, y1 = list(map(int, sys.stdin.readline().split(" ")))
-d2, m2, y2 = list(map(int, sys.stdin.readline().split(" ")))
-
-if y1 > y2:
-    print(10000)
-elif y1 == y2:
-    if m1 > m2:
-        print ((m1-m2) * 500)
-    elif m1 == m2:
-        if d1 > d2:
-            print ((d1-d2) * 15)
-        else:
+    def main(args: Array[String]) {
+        var Array(d1, m1, y1) = scala.io.StdIn.readLine().split(" ").map(_.toInt);
+        var Array(d2, m2, y2) = scala.io.StdIn.readLine().split(" ").map(_.toInt);
+        
+        if (y1 > y2)
+            print(10000)
+        else if (y1 == y2) {
+            if (m1 > m2)
+                print ((m1-m2) * 500)
+            else if (m1 == m2)
+                if (d1 > d2)
+                    print ((d1-d2) * 15)
+                else
+                    print (0)
+            else
+                print (0)
+        }
+        else
             print (0)
-    else:
-        print (0)
-else:
-    print (0)
+    }
+}
