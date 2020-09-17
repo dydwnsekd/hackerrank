fb7c460110f635b1173ac0b1f402f6c7cf440f6d
dydwnsekd <dydwnsekd123@gmail.com>
2020-09-17 20:02:04 +0900

    Bit wise AND 공식 이용하여 완료

--- python/Day29_BitwiseAND.py
+++ python/Day29_BitwiseAND.py
@@ -6,23 +6,16 @@ import random
 import re
 import sys
 
-
-
 if __name__ == '__main__':
     t = int(input())
 
     for t_itr in range(t):
-        max_num = -1 
         nk = input().split()
 
         n = int(nk[0])
         k = int(nk[1])
 
-        for i in range(n, 1, -1):
-            for j in range(1, i):
-                if max_num < i&j < k:
-                    max_num = i&j
-        
-        print (max_num)
-
-        
+        if (k-1) | k <= n:
+            print(k-1)
+        else:
+            print(k-2)
