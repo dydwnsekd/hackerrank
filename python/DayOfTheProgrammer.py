8dd48256b46298cd16db27816bb97fde9726c989
dydwnsekd <dydwnsekd123@gmail.com>
2020-10-16 19:29:34 +0900

    Day of the Programmer python으로 완료 윤년 처리

--- python/DayOfTheProgrammer.py
+++ python/DayOfTheProgrammer.py
@@ -5,13 +5,15 @@ import os
 import random
 import re
 import sys
-from datetime import datetime, timedelta
 
 # Complete the dayOfProgrammer function below.
 def dayOfProgrammer(year):
-    dt = datetime.strptime(str(year), "%Y")
-    dt += timedelta(days=255)
-    return dt.strftime("%d.%m.%Y")
+    if (year == 1918):
+        return '26.09.1918'
+    elif ((year <= 1917) & (year%4 == 0)) or ((year > 1918) & (year%400 == 0 or ((year%4 == 0) & (year%100 != 0)))):
+        return '12.09.%s' %year
+    else:
+        return '13.09.%s' %year
 
 
 if __name__ == '__main__':
