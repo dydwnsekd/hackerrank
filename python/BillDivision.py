a582c4e0c81dc309945e1b8822f1d9c66abd71f1
dydwnsekd <dydwnsekd123@gmail.com>
2020-10-17 11:23:09 +0900

    Bill Division python 완료

---
+++ python/BillDivision.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the bonAppetit function below.
+def bonAppetit(bill, k, b):
+    Anna_bill = int((sum(bill) - bill[k]) / 2)
+
+    if b == Anna_bill:
+        print("Bon Appetit")
+    else:
+        print(b - Anna_bill)
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
