27f2b0198aad3ac87612ba31a56768d2c3ff1352
dydwnsekd <dydwnsekd123@gmail.com>
2020-10-27 23:02:42 +0900

    plus minus scala 완료

---
+++ scala/PlusMinus.scala
@@ -0,0 +1,43 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+
+object Solution {
+
+    // Complete the plusMinus function below.
+    def plusMinus(arr: Array[Int]) {
+        var plus_count = 0.0d
+        var zero_count = 0.0d
+        var minus_count = 0.0d
+        val arr_size = arr.size
+
+        for (i <- arr) {
+            if (i > 0)
+                plus_count += 1
+            else if (i == 0)
+                zero_count += 1
+            else
+                minus_count += 1
+        }
+
+        printf("%f\n", (plus_count/arr_size))
+        printf("%f\n", (minus_count/arr_size))
+        printf("%f\n", (zero_count/arr_size))
+
+    }
+
+    def main(args: Array[String]) {
+        val stdin = scala.io.StdIn
+
+        val n = stdin.readLine.trim.toInt
+
+        val arr = stdin.readLine.split(" ").map(_.trim.toInt)
+        plusMinus(arr)
+    }
+}
