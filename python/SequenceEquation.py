d4c66b8de23511db90e18a4d5939ad93f012f8ca
dydwnsekd <dydwnsekd123@gmail.com>
2020-12-16 22:20:39 +0900

    Sequence Equation python 완료

---
+++ python/SequenceEquation.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the permutationEquation function below.
+def permutationEquation(p):
+    return_list = []
+
+    for i in range(1, len(p)+1):
+        return_list.append(p.index(p.index(i)+1)+1)
+        
+    return return_list
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
