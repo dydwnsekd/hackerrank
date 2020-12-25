89331ae3a2f025948bc416f6200a4498d9e0df9a
dydwnsekd <dydwnsekd123@gmail.com>
2020-12-25 23:53:39 +0900

    Lonely Integer python 완료

---
+++ python/LonelyInteger.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the lonelyinteger function below.
+def lonelyinteger(a):
+    set_a = set(a)
+    for i in set_a:
+        if a.count(i) == 1:
+            return i
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
