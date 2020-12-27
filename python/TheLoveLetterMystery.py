a64417ad91e9f177233eb40c083cf294e1698cf9
dydwnsekd <dydwnsekd123@gmail.com>
2020-12-27 23:46:05 +0900

    The Love-Letter Mystery python 완료

---
+++ python/TheLoveLetterMystery.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the theLoveLetterMystery function below.
+def theLoveLetterMystery(s):
+    count = 0
+    reverse_s = s[::-1]
+            
+    for i in range(int(len(s)/2)):
+        count += abs(ord(reverse_s[i]) - ord(s[i]))
+        
+    return count
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
