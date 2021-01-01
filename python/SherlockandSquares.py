7bf6e1cdaf02cc4e26c0f35e8e61163b41fc3cba
dydwnsekd <dydwnsekd123@gmail.com>
2021-01-01 23:27:12 +0900

    Sherlock and Squares python 완료

---
+++ python/SherlockandSquares.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the squares function below.
+def squares(a, b):
+    result = math.floor(math.sqrt(b)) - math.ceil(math.sqrt(a))
+    if result < 0:
+        return 0
+    else:
+        return result + 1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        ab = input().split()
+
+        a = int(ab[0])
+
+        b = int(ab[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
