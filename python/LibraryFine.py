09519be65d8be28097e07dd54f6d6b87b477fe2d
dydwnsekd <dydwnsekd123@gmail.com>
2021-01-06 21:03:35 +0900

    Library Fine python 완료

--- python/LibraryFine.py
+++ python/LibraryFine.py
@@ -10,13 +10,12 @@ import sys
 def libraryFine(d1, m1, y1, d2, m2, y2):
     if y1 > y2:
         return 10000
-    elif m1 > m2:
+    elif m1 > m2 and y1 == y2:
         return (m1-m2) * 500
-    elif d1 > d2:
+    elif d1 > d2 and y1 == y2 and m1 == m2:
         return (d1-d2) * 15
     else:
         return 0
-    
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
