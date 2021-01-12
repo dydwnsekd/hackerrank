7e684cb01715282efb72a640815d39aa7b85c1d8
dydwnsekd <dydwnsekd123@gmail.com>
2021-01-12 21:49:17 +0900

    Repeated String scala 일부 테스트 케이스 실패

---
+++ scala/RepeatedString.scala
@@ -0,0 +1,37 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+
+object Solution {
+
+    // Complete the repeatedString function below.
+    def repeatedString(s: String, n: Long): Long = {
+        val result = (s.count(_ == 'a') * n/s.length) + 
+        (s.substring(0, (n % s.length).toInt).count(_ == 'a'))
+        
+        return result
+
+    }
+
+    def main(args: Array[String]) {
+        val stdin = scala.io.StdIn
+
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val s = stdin.readLine
+
+        val n = stdin.readLine.trim.toLong
+
+        val result = repeatedString(s, n)
+
+        printWriter.println(result)
+
+        printWriter.close()
+    }
+}
