f697016d662189353dfc7e6d1981e041e51c8e7c
dydwnsekd <dydwnsekd123@gmail.com>
2021-01-19 21:30:38 +0900

    Cut the sticks scala SortedSet이용 완료

--- scala/Cutthesticks.scala
+++ scala/Cutthesticks.scala
@@ -7,24 +7,22 @@ import java.util.concurrent._
 import java.util.function._
 import java.util.regex._
 import java.util.stream._
+import scala.collection.immutable.SortedSet
 import scala.collection.mutable.ArrayBuffer
 
 object Solution {
 
     // Complete the cutTheSticks function below.
     def cutTheSticks(arr: Array[Int]): Array[Int] = {
-        val set_arr = arr.sorted.toSet
+        val set_arr = SortedSet[Int]() ++ arr.toSet
         var count = arr.length
         var result_arr = ArrayBuffer(count)
-        
-        println(set_arr)
         
         for (i <- set_arr.slice(0, set_arr.size-1)) {
             result_arr += (count-arr.count(_ == i))
             count -= (arr.count(_ == i))
         }
         return result_arr.toArray
-
     }
 
     def main(args: Array[String]) {
