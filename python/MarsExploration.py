634ea279404c2dc6f2c089302fac8291aea8f23f
dydwnsekd <dydwnsekd123@gmail.com>
2021-01-23 23:01:36 +0900

    Mars Exploration python 완료

---
+++ python/MarsExploration.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the marsExploration function below.
+def marsExploration(s):
+    SOS_String = "SOS"
+    count = 0
+    for i in range(len(s)):
+        if s[i] != SOS_String[i%3]:
+            count += 1
+            
+    return count
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
