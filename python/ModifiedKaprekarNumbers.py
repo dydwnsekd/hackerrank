116cfbf576802ea2b2a7f98f1768423568a9854d
dydwnsekd <dydwnsekd123@gmail.com>
2021-02-09 22:42:23 +0900

    Modified Kaprekar Numbers python 완료

--- python/ModifiedKaprekarNumbers.py
+++ python/ModifiedKaprekarNumbers.py
@@ -11,14 +11,21 @@ def kaprekarNumbers(p, q):
     result_list = list()
     for i in range(p, q+1):
         temp = str(i ** 2)
-        temp_1 = int(temp[:len(temp)//2])
-        temp_2 = int(temp[len(temp)//2:])
+        if len(temp) == 1:
+            temp_1 = int(temp)
+            temp_2 = 0
+        else:
+            temp_1 = int(temp[:len(temp)//2])
+            temp_2 = int(temp[len(temp)//2:])
         if temp_1 + temp_2 == i:
             result_list.append(i)
         
-    for i in result_list:
-        print(i, end=" ")
-        
+    if len(result_list) == 0:
+        print("INVALID RANGE")
+    else:
+        for i in result_list:
+            print(i, end=" ")
+
 if __name__ == '__main__':
     p = int(input())
 
