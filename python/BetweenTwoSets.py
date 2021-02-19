cf71aa503887f1814822d4ca33b674ffdefd3de3
dydwnsekd <dydwnsekd123@gmail.com>
2021-02-19 22:56:44 +0900

    Between Two Sets python 최대공약수 최소공배수 이용 숫자가 2개 이상인 경우 처리 필요

---
+++ python/BetweenTwoSets.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+def getGCD(num1, num2):
+    r = -1
+    if num1 < num2: num1, num2 = num2, num1
+
+    while (r!=0):
+        r = num1 % num2
+        num1, num2 = num2, r
+
+    return(num1)
+    
+def getLCM(num1, num2):
+    return int((num1 * num2) / getGCD(num1, num2))
+
+def getTotalX(a, b):
+    # Write your code here
+    count = 0
+    lcm = getLCM(a[0], a[1])
+    for i in b:
+        if i % lcm == 0:
+            count += 1
+    
+    return count
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
