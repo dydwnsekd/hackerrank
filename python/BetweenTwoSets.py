826d7752a926d66c1916efe695f530d03f8e1639
dydwnsekd <dydwnsekd123@gmail.com>
2021-02-20 11:16:03 +0900

    Between Two Sets python 완료

--- python/BetweenTwoSets.py
+++ python/BetweenTwoSets.py
@@ -14,6 +14,7 @@ import sys
 #  1. INTEGER_ARRAY a
 #  2. INTEGER_ARRAY b
 #
+
 def getGCD(num1, num2):
     r = -1
     if num1 < num2: num1, num2 = num2, num1
@@ -30,13 +31,22 @@ def getLCM(num1, num2):
 def getTotalX(a, b):
     # Write your code here
     count = 0
-    lcm = getLCM(a[0], a[1])
-    for i in b:
-        if i % lcm == 0:
+    if len(a) >= 2:
+        lcm = getLCM(a[0], a[1])
+        for i in range(2, len(a)):
+            lcm = getLCM(a[i], lcm)
+    else:
+        lcm = a[0]
+    
+    gcd = b[0]
+    for i in range(1, len(b)):
+        gcd = getGCD(b[i], gcd)
+        
+    for i in range(1, (gcd // lcm) + 1):
+        if gcd % (lcm * i) == 0:
             count += 1
-    
+            
     return count
-    
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
