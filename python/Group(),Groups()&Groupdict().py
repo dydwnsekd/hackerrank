7b6984d701f106e2b9f84c7cd3d941e778cb5166
dydwnsekd <dydwnsekd123@gmail.com>
2021-11-02 00:23:15 +0900

    Group(), Groups() & Groupdict() 완료

--- python/Group(),Groups()&Groupdict().py
+++ python/Group(),Groups()&Groupdict().py
@@ -1,5 +1,6 @@
 import re
-regex_string = re.search(r'([a-zA-Z0-9])', input().strip())
+regex_string = re.search(r'([a-zA-Z0-9])\1+', input().strip())
 if regex_string:
     print(regex_string.group(1))
-else: -1
+else:
+    print(-1)
