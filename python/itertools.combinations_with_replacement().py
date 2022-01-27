3f42811a39e17f98b1cb28a614b53b5a1ce2f318
dydwnsekd <dydwnsekd123@gmail.com>
2022-01-28 00:37:28 +0900

    itertools.combinations_with_replacement() 완료

--- python/itertools.combinations_with_replacement().py
+++ python/itertools.combinations_with_replacement().py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
     a, b = sys.stdin.readline().split()
 
     result = list(combinations_with_replacement(a, int(b)))
+    for i in range(len(result)):
+        result[i] = sorted(result[i])
     result = sorted(result)
 
     for i in result:
