6264cd5bcee2dc91d6d65e224215fefbe47ee4e4
dydwnsekd <dydwnsekd123@gmail.com>
2022-02-14 23:41:21 +0900

    Handshake python 완료

---
+++ python/Handshake.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'handshake' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def handshake(n):
+    result = 0
+    
+    for i in range(n):
+        result += i
+        
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = handshake(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
