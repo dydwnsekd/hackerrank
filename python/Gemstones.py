4cde2ee3b3bd79b278eff762d7e72a1204599982
dydwnsekd <dydwnsekd123@gmail.com>
2022-03-26 11:27:01 +0900

    gemstones python 완료

---
+++ python/Gemstones.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gemstones' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING_ARRAY arr as parameter.
+#
+
+def gemstones(arr):
+    set_arr = []
+    for i in arr:
+        set_arr.append(set(i))
+    
+    result = set_arr[0]
+    for i in set_arr[1:]:
+        result = i & result
+
+    return len(result)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+
+    result = gemstones(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
