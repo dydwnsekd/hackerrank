dcaab32b904dcf6ff7c17fc9c199a6537a4b856d
dydwnsekd <dydwnsekd123@gmail.com>
2022-05-06 23:38:29 +0900

    Strange Counter python 완료

---
+++ python/Strange_Counter.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'strangeCounter' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER t as parameter.
+#
+
+def strangeCounter(t):
+    init_t = 3
+    while t > init_t:
+        t = t-init_t
+        init_t *= 2
+
+    return init_t-t+1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    result = strangeCounter(t)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
