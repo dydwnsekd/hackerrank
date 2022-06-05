d1b9baeb61a8b95c1204d1b853328f34e12959ac
dydwnsekd <dydwnsekd123@gmail.com>
2022-06-05 23:41:06 +0900

    set Mutations init set_mutations function

--- python/SetMutations.py
+++ python/SetMutations.py
@@ -1,12 +1,28 @@
 import sys
 
+def set_mutations(s ,command, args):
+    if command == "update":
+        s.update(args)
+        return s
+    elif command == "intersection_update":
+        s.intersection_update(args)
+        return s
+    elif command == "difference_update":
+        s.difference_update(args)
+        return s
+    elif command == "symmetric_difference_update":
+        s.symmetric_difference_update(args)
+        return s
+
 sys.stdin.readline()
-set_arr = list(map(int, sys.stdin.readline().split()))
+set_arr = set(map(int, sys.stdin.readline().split()))
 
 n = int(sys.stdin.readline())
 
 for _ in range(n):
-    command, args_count = sys.stdin.readline().split()
-    print(command, args_count)
+    command = sys.stdin.readline().split()[0]
+    args = list(map(int, sys.stdin.readline().split()))
     
-    sys.stdin.readline()
+    set_arr = set_mutations(set_arr, command, args)
+    
+    print(set_arr)
