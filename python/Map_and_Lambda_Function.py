32a4d755b54f8297b18018e90552533dc58dc045
dydwnsekd <dydwnsekd123@gmail.com>
2022-06-10 00:36:22 +0900

    Map and Lambda Function 완료

--- python/Map_and_Lambda_Function.py
+++ python/Map_and_Lambda_Function.py
@@ -1,20 +1,20 @@
-cube = lambda x: x**2
+cube = lambda x: pow(x,3)
 
 def fibonacci(n):
+    answer = []
+    for i in range(n):
+        answer.append(make_fibo_num(i))
+
+    return answer
+    
+def make_fibo_num(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
-def fibo_list(n):
-    answer = []
-    for i in range(n):
-        answer.append(fibonacci(i))
-
-    return answer
+        return make_fibo_num(n-1) + make_fibo_num(n-2)
 
 if __name__ == '__main__':
     n = int(input())
-    print(list(map(cube, fibo_list(n))))
+    print(list(map(cube, fibonacci(n))))
