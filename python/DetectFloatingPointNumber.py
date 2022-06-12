2259a9f67cbb7116a9c4e838e768c8c79d9e94f3
dydwnsekd <dydwnsekd123@gmail.com>
2022-06-13 00:02:30 +0900

    Detect Floating Point Number 완료

--- python/DetectFloatingPointNumber.py
+++ python/DetectFloatingPointNumber.py
@@ -3,12 +3,12 @@ import re
 
 n = int(sys.stdin.readline())
 
-p = re.compile("[+|-]*[1-9]+[0-9]*[.]([0-9]*)")
+p = re.compile("[+-]?[0-9]*\.[0-9]+$")
 
 for _ in range(n):
     t = sys.stdin.readline()
     if p.match(t):
-        print("true")
+        print("True")
     else:
-        print("false")
+        print("False")
     
