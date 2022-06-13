80f7fdd9b72588b1b48bfc9bbf51821061ab54e5
dydwnsekd <dydwnsekd123@gmail.com>
2022-06-14 00:54:19 +0900

    Validating phone numbers 일부 테스트 케이스 실패

---
+++ python/ValidatingPhoneNumbers.py
@@ -0,0 +1,13 @@
+import re
+import sys
+
+n = int(sys.stdin.readline())
+p = re.compile("^[789][0-9]{9}")
+
+for _ in range(n):
+    t = sys.stdin.readline()
+    if p.match(t):
+        print("YES")
+    else:
+        print("NO")
+    
