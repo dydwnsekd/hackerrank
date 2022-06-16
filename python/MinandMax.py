11408cf0dc43d57ce54c4bc5c662add34216529f
dydwnsekd <dydwnsekd123@gmail.com>
2022-06-17 01:41:37 +0900

    Min and Max 완료

--- python/MinandMax.py
+++ python/MinandMax.py
@@ -1,16 +1,12 @@
-import numpy
+import numpy as np
 import sys
 
 n, m = list(map(int, sys.stdin.readline().split()))
-np_array = numpy.empty((0, m), int)
-print(n)
-print(m)
+np_array = np.empty((0, m), int)
 
 for i in range(n):
     temp_list = []
     temp_list.append(list(map(int, sys.stdin.readline().split())))
-    np_arr = numpy.append(np_array, numpy.array(temp_list), axis=0)
+    np_array = np.append(np_array, np.array(temp_list), axis=0)
     
-    print(np_arr)
-    
-print (np_array)
+print (np.max(np.min(np_array, axis = 1)))
