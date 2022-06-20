65372735dfc713d80cfd8f81121650a1ae35de76
dydwnsekd <dydwnsekd123@gmail.com>
2022-06-21 00:46:01 +0900

    ZerosandOnes.py 진짜 완료

--- python/ZerosandOnes.py
+++ python/ZerosandOnes.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys
 
-n, m, p = list(map(int, sys.stdin.readline().split()))
+nums = list(map(int, sys.stdin.readline().split()))
 
-print(np.zeros((n,m,p), dtype = np.int32))
-print(np.ones((n,m,p), dtype = np.int32))
+print(np.zeros(nums, dtype=np.int32))
+print(np.ones(nums, dtype=np.int32))
