eafd45b1bcf04d38841bc5ed5e0bccf3d8ede3f2
dydwnsekd <dydwnsekd123@gmail.com>
2022-06-22 22:08:23 +0900

    Concatenate 완료

---
+++ python/Concatenate.py
@@ -0,0 +1,8 @@
+import numpy as np
+import sys
+
+n, m, p = map(int, sys.stdin.readline().split())
+np_arr1 = np.array([sys.stdin.readline().split() for _ in range(n)],int)
+np_arr2 = np.array([sys.stdin.readline().split() for _ in range(m)],int)
+
+print(np.concatenate((np_arr1, np_arr2), axis = 0))
