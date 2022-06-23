e31fee3296dae03c6e40c90069db2fa27997d9d5
dydwnsekd <dydwnsekd123@gmail.com>
2022-06-24 01:17:19 +0900

    Transpose and Flatten 완료

---
+++ python/TransposeandFlatten.py
@@ -0,0 +1,13 @@
+import numpy as np
+import sys
+
+n, m = list(map(int, sys.stdin.readline().split()))
+np_array = np.empty((0, m), int)
+
+for i in range(n):
+    temp_list = []
+    temp_list.append(list(map(int, sys.stdin.readline().split())))
+    np_array = np.append(np_array, np.array(temp_list), axis=0)
+
+print(np.transpose(np_array))
+print(np_array.flatten())
