7c8f5fb6e7f3d8dc8964e65d4081e3c6adbf28e2
dydwnsekd <dydwnsekd123@gmail.com>
2022-06-28 00:59:45 +0900

    Validation UID 완료

--- python/ValidatingUID.py
+++ python/ValidatingUID.py
@@ -3,11 +3,12 @@ import re
 
 n = int(sys.stdin.readline())
 p = re.compile("[a-zA-Z0-9]{10}")
-
+alpha = re.compile("[A-Z]")
+num = re.compile("[0-9]")
 
 for _ in range(n):
     t = sys.stdin.readline()
-    if p.match(t) and len(set(t)) == len(t):
+    if p.match(t) and len(set(t)) == len(t) and len(alpha.findall(t)) >= 2 and len(num.findall(t)) >= 3:
         print("Valid")
     else:
         print("Invalid")
